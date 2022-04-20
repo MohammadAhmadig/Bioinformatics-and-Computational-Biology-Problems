fc8e574ac42c70cc8bce21310818f9b264d2db32
Mohammad Ahmadi <94083873+MohammadAhmadig@users.noreply.github.com>
2022-04-20 19:47:08 +0430

    Delete 1 directory

--- 1/perm2.m
+++
@@ -1,8 +0,0 @@
-
-
-%load('twoPerms.mat');
-%TWO = TWO';
-%[x,y]=find(cellfun(@strcmp , TWO,cellstr(repmat(cellstr(repmat('aa',40000,1)),1,250))));
-[x,y]=find(ismember(TWO,'aa'));
-f(x,y)=2;% tool mikeshe
-%f(ind) = 1;
